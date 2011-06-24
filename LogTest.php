aea42b3e922e02b10542c61e2422062baaa32e70
Sándor Czettner <sandor@czettner.hu>
2011-06-24 22:24:02 +0200

    Initial commit

---
+++ LogTest.php
@@ -0,0 +1,10 @@
+<?php  
+require_once('Log.php');  
+class LogTest extends PHPUnit_Framework_TestCase  
+{
+	var $log;
+	public function setUp(){ }  
+	public function tearDown(){ }
+	public function testLogFileExists(){ }
+	public function testDontPurgeLogFile(){ }
+}
