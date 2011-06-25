7641ee0ffaa0c82a550a156448395bf3bcfc3686
Sándor Czettner <sandor@czettner.hu>
2011-06-25 22:14:52 +0200

    Nem kell a var

--- Log.php
+++ Log.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Documentation, License etc.
- *
- * @package Log
- */
-
+ 
+Class Log {
+	public $filename;
+	private $file;
+ 
+	public function __construct($filename) {
+		$this->filename = $filename;
+		$this->file = fopen($filename, 'a');
+	}
+ 
+	public function write($line) {
+		fwrite($this->file, $line.PHP_EOL);
+	}
+ 
+	public function __destruct() {
+		fclose($this->file);
+	}
+ 
+}
