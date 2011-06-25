c887e0650eb16a120f416c2999a839f5f3b830ec
Sándor Czettner <sandor@czettner.hu>
2011-06-25 10:05:25 +0200

    initial project import from KDevelop

--- Log.php
+++ Log.php
@@ -1,20 +1,7 @@
 <?php
+/**
+ * Documentation, License etc.
+ *
+ * @package Log
+ */
 
-Class Log {
-	var $filename;
-	var $file;
-
-	public function __construct($filename) {
-		$this->filename = $filename;
-		$this->file = fopen($filename, 'a');
-	}
-
-	public function write($line) {
-		fwrite($this->file, $line.PHP_EOL);
-	}
-
-	public function __destruct() {
-		fclose($this->file);
-	}
-
-}
